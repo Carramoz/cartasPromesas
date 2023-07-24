2c8183292a3bce227a33fac79c056e3cc78144a0
Carlos Ramos <carramoz@hotmail.com>
2023-07-24 10:54:29 -0600

    Se corrigio

--- js/main.js
+++ js/main.js
@@ -21,12 +21,31 @@ function createCards(prods) {
         mainProds.insertAdjacentHTML("beforeend",`
         <div id="1" class="card .h-100" style="width: 25%;">
                 <img src="${producto.image}" class="card-img-top" alt="...">
-                <div class="card-body">
+            <div class="card-body">
                 <h5 class="card-title">${producto.title}</h5>
                 <p class="card-text">${producto.description}</p>
                 <p class="card-text"><small class="text-body-secondary"><strong>$${producto.price}</strong></small></p>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Ver mas
+                </button>
+            </div>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">${producto.title}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-            </div>`)
+                <div class="modal-body">
+                    ${producto.description}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+                </div>
+            </div>
+        </div>`)
     });
-}//createCards\
+}//createCards
 getData();
